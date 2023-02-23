c27366c86e7dcce736c4d144d2079d1715dd6cea
HAL703 <eonsago703@protonmail.ch>
2023-02-23 09:49:54 -0500

    Added the task model for drag-and-drop

---
+++ Shared/Classes/TaskModel.cs
@@ -0,0 +1,21 @@
+﻿namespace Classes
+{
+    public class TaskModel
+    {
+        public int Id { get; set; }
+
+        public TaskStatuses Status { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime LastUpdated { get; set; }
+
+    }
+
+    public enum TaskStatuses
+    {
+        Todo,
+        In_Progress,
+        Completed
+    }
+}
