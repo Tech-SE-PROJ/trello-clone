0d1b295a40e6e8e52ebc931f1ad3f17786775bbf
BroxtonButler <broxtonbutler@outlook.com>
2023-04-06 09:33:06 -0400

    Added basic TaskCard component to display in the columns.
    added a datareader extension to safely retrieve database info.

---
+++ Server/Controllers/TaskCardController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using trello_clone.Client.Pages;
+using trello_clone.Server.Interfaces;
+using trello_clone.Server.Services;
+using trello_clone.Shared.Classes;
+
+namespace trello_clone.Server.Controllers
+{
+    public class TaskCardController : Controller
+    {
+        private ITaskCardService _taskCardService = new TaskCardService();
+
+
+
+        [HttpGet("AddTaskCard/{taskCardName}/{columnId}/{cardIndex}")]
+        public IEnumerable<TaskCard> AddTaskCard(string taskCardName, Guid columnId, int cardIndex)
+        {
+            _taskCardService.AddTaskCard(columnId, taskCardName, cardIndex);
+            
+            return _taskCardService.GetTaskCards(columnId);
+        }
+    }
+}
--- Shared/Classes/User.cs
+++ Shared/Classes/User.cs
@@ -8,11 +8,11 @@ namespace trello_clone.Shared.Classes
 {
     public class User
     {
-        public Guid Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string JobTitle { get; set; }
-        public string Email { get; set; }
-        public Guid TeamId { get; set; }
+        public Guid? Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? JobTitle { get; set; }
+        public string? Email { get; set; }
+        public Guid? TeamId { get; set; }
     }
 }
