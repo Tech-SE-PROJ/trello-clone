66692bac400c5577bde257589911088ceecdb2ec
HAL703 <eonsago703@protonmail.ch>
2023-04-12 22:07:40 -0400

    Updated AuthController to accomodate full authentication, still need password encryption

--- Server/Controllers/AuthController.cs
+++ Server/Controllers/AuthController.cs
@@ -1,12 +1,5 @@
-﻿using Azure;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Data.SqlClient;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
-using trello_clone.Server.Interfaces;
-using trello_clone.Server.Services;
-using trello_clone.Shared;
 using trello_clone.Shared.Classes;
 using static MudBlazor.Icons;
 
@@ -17,12 +10,10 @@ namespace trello_clone.Server.Controllers
     public class AuthController : ControllerBase
     {
         private readonly AppDbContext db;
-        private ILoginService _loginService;
 
         public AuthController(AppDbContext db)
         {
             this.db = db;
-            _loginService = new LoginService(db);
         }
 
         [HttpGet]
@@ -34,7 +25,6 @@ namespace trello_clone.Server.Controllers
         [HttpPost]
         public async Task<User> Post([FromBody] User user)
         {
-            //var u = db.users.FirstOrDefault(x => x.userName == user.userName);
             if (db.users.Any(u => u.userName == user.userName)) //checking if a username already exists
             {
                 return null;
@@ -64,24 +54,8 @@ namespace trello_clone.Server.Controllers
                                     .FirstOrDefault();
                     return foundUser;
                 }
-                //EntityEntry<User> entry = await db.users.FindAsync();
-                ////var foundUser = db.users.Where(u => u.userName. == user.userName);
-                //return _context.HttpContext.User.Identity.Name;
-                //StateContainer.LoggedInUser = user;
-                //return 
             }
         }
-        //private bool Search(User user)
-        //{
-        //    var foundUser = db.users.Any(u => u.userName == user.userName);
-        //    if (user.userName.Contains(searchString, StringComparison.OrdinalIgnoreCase)
-        //        || customer.LastName.Contains(searchString, StringComparison.OrdinalIgnoreCase)
-        //        || customer.PhoneNumber.Contains(searchString, StringComparison.OrdinalIgnoreCase))
-        //    {
-        //        return true;
-        //    }
-        //    return false;
-        //}
 
         //[HttpGet("{id}")]
         //public async Task<User> GetById(Guid id)
@@ -113,30 +87,6 @@ namespace trello_clone.Server.Controllers
         //        await db.SaveChangesAsync();
         //    }
         //    return user;
-        //}
-
-        //[HttpGet]
-        //public async Task<IEnumerable<User>> Get(string name)
-        //{
-        //    return await Task.Factory.StartNew<IEnumerable<User>>(() =>
-        //    {
-        //        if (string.IsNullOrEmpty(name))
-        //            return db.User;
-        //        else
-        //            return db.User.Where(user => user.Username.Contains(name));
-        //    });
-        //}
-
-        //[HttpGet("Register/{userName}/{userPass}/{userEmail}")]
-        //public void Register(string userName, string userPass, string userEmail)
-        //{
-        //    _loginService.Register(userName, userPass, userEmail);
-        //}
-
-        //[HttpGet("Login/{userName}/{userPass}")]
-        //public void Login(string userName, string userPass)
-        //{
-        //    _loginService.GetUserAccount(userName, userPass);
         //}
     }
 }
