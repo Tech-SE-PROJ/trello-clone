c94a3d442a626654dcf0acf6bec451208a48e129
HAL703 <eonsago703@protonmail.ch>
2023-04-12 22:00:39 -0400

    Added a service for localStorage

--- Client/Program.cs
+++ Client/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using trello_clone.Client;
 using trello_clone.Shared;
 using MudBlazor.Services;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,7 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
-
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddSingleton<StateContainer>();
 
 await builder.Build().RunAsync();
