e001edb8e13ddfb27995482102f5b6b7a84ee28c
Abrham Dagnew <AHDagnew02@indianatech.net>
2023-04-18 11:09:54 -0400

    Added a calendar

---
+++ Shared/Classes/DayEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trello_clone.Shared.Classes
+{
+    public class DayEvent
+    {
+        public int Dayeventid { get; set; }
+        public string Note { get; set; }
+        public DateTime EventDate { get; set; } = new DateTime(1900 ,1 ,1);
+
+        public DateTime FromDate { get; set; } = new DateTime(1900, 1, 1);
+        public DateTime ToDate { get; set; } = new DateTime(1900, 1, 1);
+
+        public string DateValue { get; set; }
+        public string DayName { get; set; }
+        public string Message { get; set; }
+    }
+}
