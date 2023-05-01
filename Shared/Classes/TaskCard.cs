1eecbee40785ab08d0f63152aeef5c01eacf1b92
HAL703 <eonsago703@protonmail.ch>
2023-05-01 01:26:09 -0400

    Added data annotations/attributes for each column

--- Shared/Classes/TaskCard.cs
+++ Shared/Classes/TaskCard.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,22 +9,39 @@ namespace trello_clone.Shared.Classes
 {
     public class TaskCard
     {
+        [Column("cardId")]
         public Guid? Id { get; set; }
+        [Column("itemName")]
         public string? Name { get; set; }
+        [Column("itemDescription")]
         public string? Description { get; set; }
+        [Column("cardIndex")]
         public int? Index { get; set; }
+        [Column("assignedUserId")]
         public Guid? AssignedUserId { get; set; }
+        [Column("columnId")]
         public Guid? ColumnId { get; set; }
+        [Column("boardId")]
         public Guid? BoardId { get; set; }
+        [Column("dateCreated")]
         public DateTime? DateCreated { get; set; }
+        [Column("lastModifiedDate")]
         public DateTime? LastModified { get; set; }
-		public string? Status { get; set; }
+        [NotMapped]
+        public string? Status { get; set; }
+        [NotMapped]
+        public string? assignedUserName { get; set; }
 
         public TaskCard() { }
 		public TaskCard(string name, string status)
         {
             Name = name;
             Status = status;
+        }
+        public TaskCard(Guid? assignedUserId, Guid cardId)
+        {
+            AssignedUserId = assignedUserId;
+            Id = cardId;
         }
     }
 }
